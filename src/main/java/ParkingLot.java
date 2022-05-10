0f0e07bdddf2ca5ee228d210516061ef8cfcf8c8
rasheed <rasheed.shaik@thoughtworks.com>
2022-05-10 21:01:47 +0530

    Refactor change sequence of methods of parking lot class

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -12,36 +12,12 @@ public class ParkingLot {
         this.capacity = ten;
     }
 
-    public ParkingService getParkingService() {
-        return this.parkingService;
-    }
-
-    public void setParkingService(ParkingService parkingService) {
-        this.parkingService = parkingService;
-    }
-
     public void park(Vehicle vehicle) {
         parkingService.parkVehicleIn(this, vehicle);
     }
 
-    public UnparkingService getUnparkingService() {
-        return this.unparkingService;
-    }
-
-    public void setUnparkingService(UnparkingService unparkingService) {
-        this.unparkingService = unparkingService;
-    }
-
     public void unpark(Vehicle vehicle) {
         this.unparkingService.unparkVehicleFrom(this, vehicle);
-    }
-
-    public NotificationService getNotificationService() {
-        return this.notificationService;
-    }
-
-    public void setNotificationService(NotificationService notificationService) {
-        this.notificationService = notificationService;
     }
 
     public void setOwner(Owner owner) {
@@ -52,6 +28,30 @@ public class ParkingLot {
         if (isFull()) {
             this.notificationService.sendNotificationTo(this.owner);
         }
+    }
+
+    public ParkingService getParkingService() {
+        return this.parkingService;
+    }
+
+    public void setParkingService(ParkingService parkingService) {
+        this.parkingService = parkingService;
+    }
+
+    public UnparkingService getUnparkingService() {
+        return this.unparkingService;
+    }
+
+    public void setUnparkingService(UnparkingService unparkingService) {
+        this.unparkingService = unparkingService;
+    }
+
+    public NotificationService getNotificationService() {
+        return this.notificationService;
+    }
+
+    public void setNotificationService(NotificationService notificationService) {
+        this.notificationService = notificationService;
     }
 
     private boolean isFull() {
