b7ec45ef629b47e9c09c52a91eb969c39c516cb8
rasheed <rasheed.shaik@thoughtworks.com>
2022-05-10 17:56:30 +0530

    Refactor introduce nested class test for parking service

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,28 +1,32 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class ParkingLotTest {
-    @Test
-    void shouldBeAbleToHaveInstanceOfParkingService() {
-        ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
+    @Nested
+    class TestingParkingService {
+        @Test
+        void shouldBeAbleToHaveInstanceOfParkingService() {
+            ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
 
-        parkingLot.setParkingService(new ParkingService());
-        ParkingService parkingService = parkingLot.getParkingService();
+            parkingLot.setParkingService(new ParkingService());
+            ParkingService parkingService = parkingLot.getParkingService();
 
-        assertTrue(parkingService instanceof ParkingService);
-    }
+            assertTrue(parkingService instanceof ParkingService);
+        }
 
-    @Test
-    void shouldBeAbleToParkAVehicleUsingParkingService() {
-        ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
-        ParkingService mockedParkingService = mock(ParkingService.class);
-        Vehicle vehicle = new Vehicle();
+        @Test
+        void shouldBeAbleToParkAVehicleUsingParkingService() {
+            ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
+            ParkingService mockedParkingService = mock(ParkingService.class);
+            Vehicle vehicle = new Vehicle();
 
-        parkingLot.setParkingService(mockedParkingService);
-        parkingLot.park(vehicle);
+            parkingLot.setParkingService(mockedParkingService);
+            parkingLot.park(vehicle);
 
-        verify(mockedParkingService, times(1)).parkVehicleIn(parkingLot, vehicle);
+            verify(mockedParkingService, times(1)).parkVehicleIn(parkingLot, vehicle);
+        }
     }
 }
