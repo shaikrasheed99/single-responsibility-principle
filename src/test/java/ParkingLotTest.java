47a071a4a9d31ea85708ae208b0d0453c358ef62
rasheed <rasheed.shaik@thoughtworks.com>
2022-05-10 18:23:55 +0530

    Refactor introduce nested class test for unparking service

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -5,30 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class ParkingLotTest {
-    @Test
-    void shouldBeAbleToHaveInstanceOfUnparkingService() {
-        ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
-
-        parkingLot.setUnparkingService(new UnparkingService());
-        UnparkingService unparkingService = parkingLot.getUnparkingService();
-
-        assertNotNull(unparkingService);
-    }
-
-    @Test
-    void shouldBeAbleToUnparkUsingUnparkingService() {
-        ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
-        UnparkingService mockedUnparkingService = mock(UnparkingService.class);
-        Vehicle vehicle = new Vehicle();
-
-        parkingLot.setUnparkingService(mockedUnparkingService);
-        parkingLot.unpark(vehicle);
-
-        verify(mockedUnparkingService, times(1)).unparkVehicleFrom(parkingLot, vehicle);
-    }
-
     @Nested
     class ParkingServiceTests {
+
         @Test
         void shouldBeAbleToHaveInstanceOfParkingService() {
             ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
@@ -49,6 +28,32 @@ public class ParkingLotTest {
             parkingLot.park(vehicle);
 
             verify(mockedParkingService, times(1)).parkVehicleIn(parkingLot, vehicle);
+        }
+
+    }
+
+    @Nested
+    class UnparkingServiceTest {
+        @Test
+        void shouldBeAbleToHaveInstanceOfUnparkingService() {
+            ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
+
+            parkingLot.setUnparkingService(new UnparkingService());
+            UnparkingService unparkingService = parkingLot.getUnparkingService();
+
+            assertNotNull(unparkingService);
+        }
+
+        @Test
+        void shouldBeAbleToUnparkUsingUnparkingService() {
+            ParkingLot parkingLot = new ParkingLot(CAPACITY.TEN);
+            UnparkingService mockedUnparkingService = mock(UnparkingService.class);
+            Vehicle vehicle = new Vehicle();
+
+            parkingLot.setUnparkingService(mockedUnparkingService);
+            parkingLot.unpark(vehicle);
+
+            verify(mockedUnparkingService, times(1)).unparkVehicleFrom(parkingLot, vehicle);
         }
     }
 }
